be9b97e7a29361f1fc5d46ea23dccc46a1d07539
Manfred Lange <manfredmlange@gmail.com>
2022-03-09 02:26:22 +0000

    add 2nd test

--- LegacyCalculator_test.py
+++ LegacyCalculator_test.py
@@ -14,6 +14,18 @@ class LegacyCalculator_test(unittest.TestCase):
       # assert
       self.assertEqual(datetime.min, result.startTime)
 
+   def test_withOneDate(self):
+      # arrange
+      calculator = LegacyCalculator()
+      dates = [ datetime.now() ]
+
+      # act
+      result = calculator.calculate(dates)
+
+      # assert
+      self.assertEqual(datetime.min, result.startTime)
+      self.assertEqual(0, result.count)
+
 
 if __name__ == '__main__':
    unittest.main()
--- PlannedStart.py
+++ PlannedStart.py
@@ -3,9 +3,13 @@ from datetime import datetime
 class PlannedStart:
    def __init__(self):
       self.startTime = datetime.min
+      self.count = 0
 
    def startTime(self):
       return self.startTime
    
    def startTime(self, startTime):
       self.startTime = startTime
+
+   def count(self):
+      return self.count
