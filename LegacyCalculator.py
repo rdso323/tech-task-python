a916105ffb888e76a535593a2dc5926961f9dad2
Rohan D'Souza <rdso323@aucklanduni.ac.nz>
2022-03-10 18:32:24 +1300

    Refactoring of LegacyCalculator predominantly

--- LegacyCalculator.py
+++ LegacyCalculator.py
@@ -4,6 +4,7 @@ from PlannedStart import PlannedStart
 
 class LegacyCalculator:
    def calculate(self, dates, requiredDays = 1):
+
       dates.sort()
 
       plannedStart = PlannedStart(datetime.min, 0)
@@ -12,17 +13,31 @@ class LegacyCalculator:
       if(len(dates) == 0):
          return plannedStart
 
-      requiredNumberInFirstWeek = requiredDays
+      # requiredNumberInFirstWeek = requiredDays 
+
+      one_week = timedelta(milliseconds = 7 * 24 * 60 * 60 * 1000)
+      countsForFirstWeek=0
+      countsForSecondWeek=0
 
       startOfFirstWeek = dates[0]
       # add a week
-      startOfSecondWeek = startOfFirstWeek + timedelta(milliseconds = 7 * 24 * 60 * 60 * 1000)
+      startOfSecondWeek = startOfFirstWeek + one_week
 
-      countsForFirstWeek = len([x for x in dates if x > startOfFirstWeek and x < startOfFirstWeek + timedelta(milliseconds = 7 * 24 * 60 * 60 * 1000)]) # add a week
+      # countsForFirstWeek = len([x for x in dates if x > startOfFirstWeek and x < startOfFirstWeek + timedelta(milliseconds = 7 * 24 * 60 * 60 * 1000)]) # add a week
 
-      countsForSecondWeek = len([x for x in dates if x > startOfSecondWeek and x < startOfSecondWeek + timedelta(milliseconds = 7 * 24 * 60 * 60 * 1000)]) # add a week
+      # countsForSecondWeek = len([x for x in dates if x > startOfSecondWeek and x < startOfSecondWeek + timedelta(milliseconds = 7 * 24 * 60 * 60 * 1000)]) # add a week
 
-      if( countsForSecondWeek > countsForFirstWeek and countsForSecondWeek >= requiredNumberInFirstWeek):
+      for x in dates:
+         if(x>=startOfFirstWeek and x<startOfSecondWeek):
+            countsForFirstWeek+=1
+         elif(x>=startOfSecondWeek and x<(startOfSecondWeek+one_week)):
+            countsForSecondWeek+=1
+
+
+      if( countsForSecondWeek > countsForFirstWeek and countsForSecondWeek >= requiredDays):
          plannedStart = PlannedStart(startOfSecondWeek, countsForSecondWeek)
+      else:
+         plannedStart = PlannedStart(startOfFirstWeek, countsForFirstWeek)
+
 
       return plannedStart
